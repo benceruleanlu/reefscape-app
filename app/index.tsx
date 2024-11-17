c5ec23fdb1b1519c5bd6e965866201aa9dceccfe
Xiong Da <lajikuang457@gmail.com>
2024-11-16 21:05:19 -0500

    checks for wi-fi connection

--- app/index.tsx
+++ app/index.tsx
@@ -1,27 +1,36 @@
 import React, { useState } from "react";
 import { Text, View, TouchableOpacity } from "react-native";
+import * as Network from 'expo-network';
 
 export default function Index() {
-  const [status, setStatus] = useState("Connection uncertain");
-  const [statusCol, setStatusCol] = useState("#000000");
+	const networkState = Network.useNetworkState();
 
-  const checkConnection = () => {
-    setStatus("Checking connection...");
-	setStatusCol("#aaaa00");
-  }
+	const [status, setStatus] = useState("Connection uncertain.");
+	const [statusCol, setStatusCol] = useState("#000000");
 
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text id="status" style={{ color: statusCol, fontSize: 20 }}>{status}</Text>
-	  <TouchableOpacity onPress={checkConnection} style={{ backgroundColor: "#000000", padding: 10, marginTop: 20, borderRadius: 5 }}>
-        <Text style={{ color: "#ffffff", fontSize: 16 }}>Check Connection</Text>
-      </TouchableOpacity>
-    </View>
-  );
+	const checkConnection = async () => {
+		setStatus("Checking connection...");
+		setStatusCol("#aaaa00");
+
+		if (!networkState.isConnected || networkState.type !== Network.NetworkStateType.WIFI) {
+			setStatus("Wi-fi is not connected!");
+			setStatusCol("#bb0000");
+			return;
+		}
+	}
+
+	return (
+		<View
+			style={{
+				flex: 1,
+				justifyContent: "center",
+				alignItems: "center",
+			}}
+		>
+			<Text style={{ color: statusCol, fontSize: 20 }}>{status}</Text>
+			<TouchableOpacity onPress={checkConnection} style={{ backgroundColor: "#000000", padding: 10, marginTop: 20, borderRadius: 5 }}>
+				<Text style={{ color: "#ffffff", fontSize: 16 }}>Check Connection</Text>
+			</TouchableOpacity>
+		</View>
+	);
 }
